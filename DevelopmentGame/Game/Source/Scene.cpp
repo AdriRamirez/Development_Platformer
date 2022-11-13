4be2804f1485585a4027d5bf080083ae435dd5ab
AdriRamirez <a99monster@gmail.com>
2022-11-13 16:27:33 +0100

    camera fix

--- DevelopmentGame/Game/Source/Scene.cpp
+++ DevelopmentGame/Game/Source/Scene.cpp
@@ -132,18 +132,15 @@ bool Scene::Update(float dt)
 		}
 	}
 		
+	if (godMode) {
+		if (app->input->GetKey(SDL_SCANCODE_UP) == KEY_REPEAT)
+			app->render->camera.y += 10;
 
-	if (app->input->GetKey(SDL_SCANCODE_UP) == KEY_REPEAT)
-		app->render->camera.y += 10;
+		if (app->input->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT)
+			app->render->camera.y -= 10;
 
-	if (app->input->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT)
-		app->render->camera.y -= 10;
-
-	if (app->input->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT)
-		app->render->camera.x += 20;
-
-	if (app->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
-		app->render->camera.x -= 20;
+	}
+	
 	
 	//Camera movement with the player
 	uint x, y;
