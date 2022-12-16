7d3993efac23b55ce675f47f424e764fbf6f988d
Bankaster <miguelroto1998@gmail.com>
2022-12-16 19:29:43 +0100

    Enemy update

--- DevelopmentGame/Game/Source/Enemy.h
+++ DevelopmentGame/Game/Source/Enemy.h
@@ -1,1 +1,35 @@
-#pragma once
+#ifndef __ENEMY_H__
+#define __ENEMY_H__
+
+#include "Entity.h"
+#include "Point.h"
+#include "SDL/include/SDL.h"
+
+struct SDL_Texture;
+
+class Floor_Enemy : public Entity
+{
+public:
+
+	Floor_Enemy();
+	virtual ~Floor_Enemy();
+
+	bool Awake();
+
+	bool Start();
+
+	bool Update();
+
+	bool CleanUp();
+
+public:
+
+private:
+
+	SDL_Texture* texture;
+	const char* texturePath;
+
+	PhysBody* pbody;
+};
+
+#endif // __ENEMY_H__
